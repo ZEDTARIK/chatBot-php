9fad367d8b46a2a61bf150a75000bf2c047ac809
zouhair <ettarak.zouhair@gmail.com>
2022-03-18 23:26:41 +0100

    ChatBotMessage automatique Js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,24 @@ var sendBtn = document.getElementById('sendBtn');
 var textBox = document.getElementById('textBox');
 var chatContainer = document.getElementById('chatContainer');
 
-// function sendMessage
+chatBotMessage("Hi From ChatBot !!!");
 
+// function chatBotMessage
+function chatBotMessage(MessageText) {
+    var messageElement = document.createElement('div');
+    messageElement.classList.add('w-50');
+    messageElement.classList.add('float-left');
+    messageElement.classList.add('shadow-sm');
+    messageElement.style.margin = "10px";
+    messageElement.style.padding = "5px";
+
+    messageElement.innerHTML = "<span>Chat: </span>" +
+        "<span style=" + "margin-top: 10px; padding: 10px;" + ">" + MessageText + "</span>";
+
+        chatContainer.appendChild(messageElement);
+
+}
+// function sendMessage
 function sendMessage(MessageText) {
 
     var messageElement = document.createElement('div');
