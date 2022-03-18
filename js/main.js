72f5e26a74f5fb881918e6ec96c1bf84ef55447f
zouhair <ettarak.zouhair@gmail.com>
2022-03-18 23:19:14 +0100

    add ListenBtn JavaScript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-// get elements 
+//elements 
 var sendBtn = document.getElementById('sendBtn');
 var textBox = document.getElementById('textBox');
 var chatContainer = document.getElementById('chatContainer');
+
+// function sendMessage
+
+function sendMessage(MessageText) {
+
+    var messageElement = document.createElement('div');
+    messageElement.classList.add('w-50');
+    messageElement.classList.add('float-right');
+    messageElement.classList.add('shadow-sm');
+    messageElement.style.margin = "10px";
+    messageElement.style.padding = "5px";
+
+    messageElement.innerHTML = "<span>You: </span>" +
+        "<span style=" + "margin-top: 10px; padding: 10px;" + ">" + MessageText + "</span>";
+
+        chatContainer.appendChild(messageElement);
+}
+
+sendBtn.addEventListener('click', function () {
+
+    let MessageText = textBox.value;
+    sendMessage(MessageText);
+});
